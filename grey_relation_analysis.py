ddfbce79e09953a008476b3e8ebdd54a9d5991e5
infinitum-rakesh <rakeshnitcalicut@gmail.com>
2014-10-25 07:42:07 +0530

    Batsmen were set :) Thanks to Grey Relation Analysis

--- grey_relation_analysis.py
+++ grey_relation_analysis.py
@@ -6,7 +6,7 @@ import xlsxwriter
 
 
 #Open data sheet
-book = open_workbook('bat/complete_batsmen_stats.xls')
+book = open_workbook('grey_relation_coefficients.xls')
 
 
 #Index data sheet
@@ -42,7 +42,7 @@ def normalize_complete_batsmen_stats():
 	hcentury = {}
 	for row_index in range(1,sheet.nrows):
 		for col_index in range(sheet.ncols):			
-			if cellname(row_index,col_index)[0] == 'A':
+			if cellname(row_index,col_index)[0] == 'A' and sheet.cell(row_index,col_index+1).value >= 20:
 				curr_batsmen = sheet.cell(row_index,col_index).value
 				runs[curr_batsmen] = sheet.cell(row_index,col_index+3).value
 				innings[curr_batsmen] = sheet.cell(row_index,col_index+1).value
@@ -91,15 +91,152 @@ def normalize_complete_batsmen_stats():
 		normalized_complete_batsmen_stats_sheet.write(row_count,5,
 																"%.2f"%((century[x] - min_c) / (max_c - min_c))
 																)
-		row_count+=1
+		row_count+=1	
+	workbook.close()
+
+
+def grc_calculation():
+	grc_rpm = {}
+	delmax_rpm = -1
+	delmin_rpm = 1
+	grc_avg = {}
+	delmax_avg = -1
+	delmin_avg = 1
+	grc_sr = {}
+	delmax_sr = -1
+	delmin_sr = 1
+	grc_c= {}
+	delmax_c = -1
+	delmin_c = 1
+	grc_hc = {}
+	delmax_hc = -1
+	delmin_hc = 1
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_rpm = float(sheet.cell(row_index,col_index+1).value)
+				if curr_rpm != 0.00:
+					if 1 - curr_rpm > delmax_rpm:
+						delmax_rpm = 1.0- curr_rpm
+				if curr_rpm != 1.00:		
+					if 1 - curr_rpm < delmin_rpm:
+						delmin_rpm 	= 1.0 - curr_rpm
+				curr_avg = float(sheet.cell(row_index,col_index+2).value)
+				if curr_avg != 0.0:
+					if 1 - curr_avg > delmax_avg:
+						delmax_avg = 1.0 - curr_avg
+				if curr_avg != 1.00:		
+					if 1 - curr_avg < delmin_avg:
+						delmin_avg 	= 1.0 - curr_avg		
+				curr_sr = float(sheet.cell(row_index,col_index+3).value)
+				if curr_sr != 0.0:
+					if 1 - curr_sr > delmax_sr:
+						delmax_sr = 1.0 - curr_sr
+				if curr_sr != 1.00:		
+					if 1 - curr_sr < delmin_sr:
+						delmin_sr 	= 1.0 - curr_sr	
+				curr_hc = float(sheet.cell(row_index,col_index+4).value)
+				if curr_hc != 0.0:
+					if 1 - curr_hc > delmax_hc:
+						delmax_hc = 1.0 - curr_hc
+				if curr_hc != 1.00:		
+					if 1 - curr_hc < delmin_hc:
+						delmin_hc 	= 1.0 - curr_hc						
+				curr_c = float(sheet.cell(row_index,col_index+5).value)
+				if curr_c != 0.0:
+					if 1 - curr_c > delmax_c:
+						delmax_c = 1.0 - curr_c
+				if curr_c != 1.00:		
+					if 1 - curr_c < delmin_c:
+						delmin_c 	= 1.0 - curr_c	
+	#print(delmax_rpm,delmin_rpm,delmax_avg,delmin_avg,delmax_sr,delmin_sr,delmax_hc,delmin_hc,delmax_c,delmin_c)					
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+		 if cellname(row_index,col_index)[0] == 'A':
+		 	curr_batsmen = sheet.cell(row_index,col_index).value
+		 	curr_rpm = float(sheet.cell(row_index,col_index+1).value)
+		 	curr_avg = float(sheet.cell(row_index,col_index+2).value)
+		 	curr_sr = float(sheet.cell(row_index,col_index+3).value)
+		 	curr_hc = float(sheet.cell(row_index,col_index+4).value)
+		 	curr_c = float(sheet.cell(row_index,col_index+5).value)
+		 	grc_rpm[curr_batsmen] = "%.2f"%((delmin_rpm + (.5 * delmax_rpm)) / ((1-curr_rpm) + .5*delmax_rpm))
+		 	grc_avg[curr_batsmen] = "%.2f"%((delmin_avg + (.5 * delmax_avg)) / ((1-curr_avg) + .5*delmax_avg))
+		 	grc_sr[curr_batsmen] = "%.2f"%((delmin_sr + (.5 * delmax_sr)) / ((1-curr_sr) + .5*delmax_sr))
+		 	grc_hc[curr_batsmen] = "%.2f"%((delmin_hc + (.5 * delmax_hc)) / ((1-curr_hc) + .5*delmax_hc))
+		 	grc_c[curr_batsmen] = "%.2f"%((delmin_c + (.5 * delmax_c)) / ((1-curr_c) + .5*delmax_c))	
+	#Create new sheet
+	workbook,grey_relation_coefficients_sheet = create_new_sheet(
+														"grey_relation_coefficients.xls"
+														)
+
+	#Initialize rows,columns
+	row_count = 0
+	grey_relation_coefficients_sheet.write(0,0,"Player Name")
+	grey_relation_coefficients_sheet.write(0,1,"Runs per innings")
+	grey_relation_coefficients_sheet.write(0,2,"AVG")
+	grey_relation_coefficients_sheet.write(0,3,"SR")
+	grey_relation_coefficients_sheet.write(0,4,"50's")
+	grey_relation_coefficients_sheet.write(0,5,"100's")
+	row_count+=1
+
+	for x in grc_rpm:
+		grey_relation_coefficients_sheet.write(row_count,0,x)
+		grey_relation_coefficients_sheet.write(row_count,1,grc_rpm[x])
+		grey_relation_coefficients_sheet.write(row_count,2,grc_avg[x])
+		grey_relation_coefficients_sheet.write(row_count,3,grc_sr[x])
+		grey_relation_coefficients_sheet.write(row_count,4,grc_hc[x])
+		grey_relation_coefficients_sheet.write(row_count,5,grc_c[x])
+		row_count+=1	
+
 	workbook.close()	
 
 
 
+def grd_calculation():
+	w_rpm = 35
+	w_avg = 30
+	w_sr = 10
+	w_hc = 10
+	w_c = 15
+	grd_batsmen = {}
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_batsmen = sheet.cell(row_index,col_index).value
+				curr_rpm = float(sheet.cell(row_index,col_index+1).value)
+			 	curr_avg = float(sheet.cell(row_index,col_index+2).value)
+			 	curr_sr = float(sheet.cell(row_index,col_index+3).value)
+		 		curr_hc = float(sheet.cell(row_index,col_index+4).value)
+		 		curr_c = float(sheet.cell(row_index,col_index+5).value)
+				grd_batsmen[curr_batsmen] = (w_rpm * curr_rpm +
+											w_avg * curr_avg +
+											w_sr * curr_sr +
+											w_hc * curr_hc +
+											w_c * curr_c)
+	#Create new sheet
+	workbook,grey_relation_grades_sheet = create_new_sheet(
+														"grey_relation_grades.xls"
+														)
+
+	#Initialize rows,columns
+	row_count = 0		
+	grey_relation_grades_sheet.write(0,0,"Player Name")
+	grey_relation_grades_sheet.write(0,1,"Grade")
+	row_count+=1
+	for x in grd_batsmen:
+		grey_relation_grades_sheet.write(row_count,0,x)
+		grey_relation_grades_sheet.write(row_count,1,grd_batsmen[x])
+		row_count+=1
+	workbook.close()	
 
 
+											
+												
+
+
+	
 				
-				
 
-
-normalize_complete_batsmen_stats()
+#normalize_complete_batsmen_stats()
+#grc_calculation()
+grd_calculation()
