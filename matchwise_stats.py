7fd68eace3fc671a148f8babcdf2d1f82c58d628
infinitum-rakesh <rakeshnitcalicut@gmail.com>
2014-10-26 14:15:22 +0530

    New Match stats added.

--- matchwise_stats.py
+++ matchwise_stats.py
@@ -80,8 +80,59 @@ def winning_probabilities(sheet_name):
 				exit()	
 			row_count+=1
 	workbook.close()
-	
-winning_probabilities("match/complete_match_stats.xls")			
+def team_avg_scores(sheet_name):
+	teams={}
+	sheet = open(sheet_name)
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(0,sheet.ncols):
+			if cellname(row_index,col_index)[0] == 'D':
+				bat1 = sheet.cell(row_index,col_index+2).value
+				bat2 = sheet.cell(row_index,col_index+3).value
+				if bat1 not in teams:
+					teams[bat1] = [0,0,0,0,0,0]
+				if bat2 not in teams:
+					teams[bat2] = [0,0,0,0,0,0]
+				score1 = float(sheet.cell(row_index,col_index+4).value)
+				wik1 = float(sheet.cell(row_index,col_index+5).value)
+				score2 = float(sheet.cell(row_index,col_index+7).value)
+				wik2 = float(sheet.cell(row_index,col_index+8).value)
+				teams[bat1][0]+=1
+				teams[bat2][5]+=1
+				teams[bat1][1]+=score1
+				teams[bat2][3]+=score2
+				teams[bat1][2]+=wik1
+				teams[bat2][4]+=wik2
+	#Create new sheet
+	workbook,team_scores_sheet = create_new_sheet("team_scores.xls")
+	#Initialize rows,columns
+	row_count = 0
+	team_scores_sheet.write(0,0,"Team Name")
+	team_scores_sheet.write(0,1,"Total No of Matches")
+	team_scores_sheet.write(0,2,"Avg score-1")
+	team_scores_sheet.write(0,3,"Avg wkt-1")
+	team_scores_sheet.write(0,4,"Avg score-2")
+	team_scores_sheet.write(0,5,"Avg wkt-2")
+	row_count+=1
+	for x in teams:
+		team_scores_sheet.write(row_count,0,x)
+		team_scores_sheet.write(row_count,1,teams[x][0]+teams[x][5])
+		if teams[x][0] != 0:
+			team_scores_sheet.write(row_count,2,teams[x][1]//teams[x][0])
+			team_scores_sheet.write(row_count,3,teams[x][2]//teams[x][0])
+		else:
+			team_scores_sheet.write(row_count,2,0)
+			team_scores_sheet.write(row_count,3,0)
+		if teams[x][5] != 0:	
+			team_scores_sheet.write(row_count,4,teams[x][3]//teams[x][5])
+			team_scores_sheet.write(row_count,5,teams[x][4]//teams[x][5])
+		else:
+			team_scores_sheet.write(row_count,4,0)
+			team_scores_sheet.write(row_count,5,0)
+		row_count+=1
+	workbook.close()	
+
+team_avg_scores("match/complete_match_stats.xls")
+#winning_probabilities("match/complete_match_stats.xls")			
 
 
 
