f6ec5077e7917aa2a659feee5b3e45cbbb4c4b07
infinitum-rakesh <rakeshnitcalicut@gmail.com>
2014-10-26 08:10:58 +0530

    World Team selected. Time for some insights now

--- grey_relation_analysis.py
+++ grey_relation_analysis.py
@@ -35,14 +35,99 @@ def min_max(a):
 	min_a = 10000000000
 	max_a = -1
 	for x in a:
-		if max_a < float(a[x]):
-			max_a = float(a[x])
-		if min_a > float(a[x]):
-			min_a = float(a[x])	
+		try:
+			if max_a < float(a[x]):
+				max_a = float(a[x])
+			if min_a > float(a[x]):
+				min_a = float(a[x])
+		except ValueError:
+			pass			
 	return (max_a,min_a)		
 
 
 #Grey Relation Analysis
+
+
+def normalize_complete_bowler_stats(yr):
+	innings = {}
+	wickets = {}
+	economy = {}
+	sr = {}
+	avg = {}
+	ftw = {}
+	wptm = {}
+	sheet = open_bowl("complete_bowler_stats"+str(yr)+".xls")
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):			
+			if cellname(row_index,col_index)[0] == 'A' and (sheet.cell(row_index,col_index+4).value >= 1
+														and sheet.cell(row_index,col_index+2).value >= 100
+														and sheet.cell(row_index,col_index+1).value >= 3):
+				curr_bowler = sheet.cell(row_index,col_index).value
+				innings[curr_bowler] = sheet.cell(row_index,col_index+1).value
+				wickets[curr_bowler] = sheet.cell(row_index,col_index+4).value
+				avg[curr_bowler] = 100.0/float(sheet.cell(row_index,col_index+5).value)
+				economy[curr_bowler] = 100.0/float(sheet.cell(row_index,col_index+6).value)
+				sr[curr_bowler] = 100.0/float(sheet.cell(row_index,col_index+7).value)
+				ftw[curr_bowler] = sheet.cell(row_index,col_index+9).value
+	for x in innings:
+		wptm[x] = (wickets[x]*10.0)/ innings[x]
+		ftw[x] = (ftw[x]*10.0)/innings[x]
+	max_wptm , min_wptm = min_max(wptm)
+	max_avg , min_avg = min_max(avg)
+	max_sr , min_sr = min_max(sr)
+	max_ftw , min_ftw = min_max(ftw)
+	max_eco , min_eco = min_max(economy)
+
+	#Create new sheet
+	workbook,normalized_complete_bowler_stats_sheet = create_new_sheet(
+														"normalized_complete_bowler_stats"+str(yr)+".xls"
+														)
+
+	#Initialize rows,columns
+	row_count = 0
+	normalized_complete_bowler_stats_sheet.write(0,0,"Player Name")
+	normalized_complete_bowler_stats_sheet.write(0,1,"Wickets per innings")
+	normalized_complete_bowler_stats_sheet.write(0,2,"AVG")
+	normalized_complete_bowler_stats_sheet.write(0,3,"SR")
+	normalized_complete_bowler_stats_sheet.write(0,4,"Economy")
+	normalized_complete_bowler_stats_sheet.write(0,5,"5W")
+	row_count+=1
+
+	for x in innings:
+		normalized_complete_bowler_stats_sheet.write(row_count,0,x)
+		if max_wptm == min_wptm:
+			normalized_complete_bowler_stats_sheet.write(row_count,1,0)
+		else:		
+			normalized_complete_bowler_stats_sheet.write(row_count,1,
+																"%.2f"%((wptm[x] - min_wptm) / (max_wptm - min_wptm))
+																)
+		if max_avg == min_avg:
+			normalized_complete_bowler_stats_sheet.write(row_count,2,0)
+		else:			
+			normalized_complete_bowler_stats_sheet.write(row_count,2,
+																"%.2f"%((float(avg[x]) - min_avg) / (max_avg - min_avg))															
+																)
+		if max_sr == min_sr:
+			normalized_complete_bowler_stats_sheet.write(row_count,3,0)
+		else:			
+			normalized_complete_bowler_stats_sheet.write(row_count,3,
+																"%.2f"%((float(sr[x]) - min_sr) / (max_sr - min_sr))
+																)
+		if max_eco == min_eco:
+			normalized_complete_bowler_stats_sheet.write(row_count,4,0)
+		else:			
+			normalized_complete_bowler_stats_sheet.write(row_count,4,
+																"%.2f"%((float(economy[x]) - min_eco) / (max_eco - min_eco))
+																)
+		if max_ftw == min_ftw:
+			normalized_complete_bowler_stats_sheet.write(row_count,5,0)
+		else:			
+			normalized_complete_bowler_stats_sheet.write(row_count,5,
+																"%.2f"%((float(ftw[x]) - min_ftw) / (max_ftw - min_ftw))
+																)
+		row_count+=1	
+	workbook.close()
+
 def normalize_complete_batsmen_stats(yr):
 	sheet = open_bat("complete_batsmen_stats"+str(yr)+".xls")
 	runs = {}
@@ -131,7 +216,101 @@ def normalize_complete_batsmen_stats(yr):
 		row_count+=1	
 	workbook.close()
 
+def bowler_grc_calculation(yr):
+	sheet = open("normalized_complete_bowler_stats"+str(yr)+".xls")
+	grc_wptm = {}
+	delmax_wptm = -1
+	delmin_wptm = 1
+	grc_avg = {}
+	delmax_avg = -1
+	delmin_avg = 1
+	grc_sr = {}
+	delmax_sr = -1
+	delmin_sr = 1
+	grc_eco= {}
+	delmax_eco = -1
+	delmin_eco = 1
+	grc_ftw = {}
+	delmax_ftw = -1
+	delmin_ftw = 1
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_wptm = float(sheet.cell(row_index,col_index+1).value)
+				if curr_wptm != 0.00:
+					if 1 - curr_wptm > delmax_wptm:
+						delmax_wptm = 1.0- curr_wptm
+				if curr_wptm != 1.00:		
+					if 1 - curr_wptm < delmin_wptm:
+						delmin_wptm 	= 1.0 - curr_wptm
+				curr_avg = float(sheet.cell(row_index,col_index+2).value)
+				if curr_avg != 0.0:
+					if 1 - curr_avg > delmax_avg:
+						delmax_avg = 1.0 - curr_avg
+				if curr_avg != 1.00:		
+					if 1 - curr_avg < delmin_avg:
+						delmin_avg 	= 1.0 - curr_avg		
+				curr_sr = float(sheet.cell(row_index,col_index+3).value)
+				if curr_sr != 0.0:
+					if 1 - curr_sr > delmax_sr:
+						delmax_sr = 1.0 - curr_sr
+				if curr_sr != 1.00:		
+					if 1 - curr_sr < delmin_sr:
+						delmin_sr 	= 1.0 - curr_sr	
+				curr_eco = float(sheet.cell(row_index,col_index+4).value)
+				if curr_eco != 0.0:
+					if 1 - curr_eco > delmax_eco:
+						delmax_eco = 1.0 - curr_eco
+				if curr_eco != 1.00:		
+					if 1 - curr_eco < delmin_eco:
+						delmin_eco 	= 1.0 - curr_eco						
+				curr_ftw = float(sheet.cell(row_index,col_index+5).value)
+				if curr_ftw != 0.0:
+					if 1 - curr_ftw > delmax_ftw:
+						delmax_ftw = 1.0 - curr_ftw
+				if curr_ftw != 1.00:		
+					if 1 - curr_ftw < delmin_ftw:
+						delmin_ftw 	= 1.0 - curr_ftw	
+	#print(delmax_rpm,delmin_rpm,delmax_avg,delmin_avg,delmax_sr,delmin_sr,delmax_hc,delmin_hc,delmax_c,delmin_c)					
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+		 if cellname(row_index,col_index)[0] == 'A':
+		 	curr_bowler = sheet.cell(row_index,col_index).value
+		 	curr_wptm = float(sheet.cell(row_index,col_index+1).value)
+		 	curr_avg = float(sheet.cell(row_index,col_index+2).value)
+		 	curr_sr = float(sheet.cell(row_index,col_index+3).value)
+		 	curr_eco = float(sheet.cell(row_index,col_index+4).value)
+		 	curr_ftw = float(sheet.cell(row_index,col_index+5).value)
+		 	grc_wptm[curr_bowler] = "%.2f"%((delmin_wptm + (.5 * delmax_wptm)) / ((1-curr_wptm) + .5*delmax_wptm))
+		 	grc_avg[curr_bowler] = "%.2f"%((delmin_avg + (.5 * delmax_avg)) / ((1-curr_avg) + .5*delmax_avg))
+		 	grc_sr[curr_bowler] = "%.2f"%((delmin_sr + (.5 * delmax_sr)) / ((1-curr_sr) + .5*delmax_sr))
+		 	grc_eco[curr_bowler] = "%.2f"%((delmin_eco + (.5 * delmax_eco)) / ((1-curr_eco) + .5*delmax_eco))
+		 	grc_ftw[curr_bowler] = "%.2f"%((delmin_ftw + (.5 * delmax_ftw)) / ((1-curr_ftw) + .5*delmax_ftw))	
+	#Create new sheet
+	workbook,grey_relation_coefficients_sheet = create_new_sheet(
+														"bowler_grey_relation_coefficients"+str(yr)+".xls"
+														)
 
+	#Initialize rows,columns
+	row_count = 0
+	grey_relation_coefficients_sheet.write(0,0,"Player Name")
+	grey_relation_coefficients_sheet.write(0,1,"Wickets per innings")
+	grey_relation_coefficients_sheet.write(0,2,"AVG")
+	grey_relation_coefficients_sheet.write(0,3,"SR")
+	grey_relation_coefficients_sheet.write(0,4,"Economy")
+	grey_relation_coefficients_sheet.write(0,5,"5W")
+	row_count+=1
+
+	for x in grc_wptm:
+		grey_relation_coefficients_sheet.write(row_count,0,x)
+		grey_relation_coefficients_sheet.write(row_count,1,grc_wptm[x])
+		grey_relation_coefficients_sheet.write(row_count,2,grc_avg[x])
+		grey_relation_coefficients_sheet.write(row_count,3,grc_sr[x])
+		grey_relation_coefficients_sheet.write(row_count,4,grc_eco[x])
+		grey_relation_coefficients_sheet.write(row_count,5,grc_ftw[x])
+		row_count+=1	
+
+	workbook.close()	
 def batsmen_grc_calculation(yr):
 	sheet = open("normalized_complete_batsmen_stats"+str(yr)+".xls")
 	grc_rpm = {}
@@ -229,7 +408,44 @@ def batsmen_grc_calculation(yr):
 	workbook.close()	
 
 
+def bowler_grd_calculation(yr):
+	sheet = open("bowler_grey_relation_coefficients"+str(yr)+".xls")
+	w_wptm = 30
+	w_avg = 15
+	w_sr = 35
+	w_eco = 10
+	w_ftw = 10
+	grd_bowler = {}
+	for row_index in range(1,sheet.nrows):
+		for col_index in range(sheet.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_bowler = sheet.cell(row_index,col_index).value
+				curr_wptm = float(sheet.cell(row_index,col_index+1).value)
+			 	curr_avg = float(sheet.cell(row_index,col_index+2).value)
+			 	curr_sr = float(sheet.cell(row_index,col_index+3).value)
+		 		curr_eco = float(sheet.cell(row_index,col_index+4).value)
+		 		curr_ftw = float(sheet.cell(row_index,col_index+5).value)
+				grd_bowler[curr_bowler] = (w_wptm * curr_wptm +
+											w_avg * curr_avg +
+											w_sr * curr_sr +
+											w_eco * curr_eco +
+											w_ftw * curr_ftw
+											)
+	#Create new sheet
+	workbook,grey_relation_grades_sheet = create_new_sheet(
+														"bowler_grey_relation_grades"+str(yr)+".xls"
+														)
 
+	#Initialize rows,columns
+	row_count = 0		
+	grey_relation_grades_sheet.write(0,0,"Player Name")
+	grey_relation_grades_sheet.write(0,1,"Grade")
+	row_count+=1
+	for x in grd_bowler:
+		grey_relation_grades_sheet.write(row_count,0,x)
+		grey_relation_grades_sheet.write(row_count,1,grd_bowler[x])
+		row_count+=1
+	workbook.close()	
 def batsmen_grd_calculation(yr):
 	sheet = open("grey_relation_coefficients"+str(yr)+".xls")
 	w_rpm = 35
@@ -267,6 +483,63 @@ def batsmen_grd_calculation(yr):
 		grey_relation_grades_sheet.write(row_count,1,grd_batsmen[x])
 		row_count+=1
 	workbook.close()	
+
+def complete_bowler_performance():
+	sheet1 = open("bowler_grey_relation_grades2011.xls")
+	sheet2 = open("bowler_grey_relation_grades2012.xls")
+	sheet3 = open("bowler_grey_relation_grades2013.xls")
+	sheet4 = open("bowler_grey_relation_grades2014.xls")
+	total_bowler_grd = {}
+	for row_index in range(1,sheet1.nrows):
+		for col_index in range(sheet1.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_bowler = sheet1.cell(row_index,col_index).value
+				curr_grade = sheet1.cell(row_index,col_index+1).value
+				if curr_bowler not in total_bowler_grd:
+					total_bowler_grd[curr_bowler] = 15*curr_grade
+	for row_index in range(1,sheet2.nrows):
+		for col_index in range(sheet2.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_bowler = sheet2.cell(row_index,col_index).value
+				curr_grade = sheet2.cell(row_index,col_index+1).value
+				if curr_bowler not in total_bowler_grd:
+					total_bowler_grd[curr_bowler] = curr_grade				
+				else:
+					total_bowler_grd[curr_bowler] += 25*curr_grade
+	for row_index in range(1,sheet3.nrows):
+		for col_index in range(sheet3.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_bowler = sheet3.cell(row_index,col_index).value
+				curr_grade = sheet3.cell(row_index,col_index+1).value
+				if curr_bowler not in total_bowler_grd:
+					total_bowler_grd[curr_bowler] = curr_grade				
+				else:
+					total_bowler_grd[curr_bowler] += 40*curr_grade					
+	for row_index in range(1,sheet4.nrows):
+		for col_index in range(sheet4.ncols):
+			if cellname(row_index,col_index)[0] == 'A':
+				curr_bowler = sheet4.cell(row_index,col_index).value
+				curr_grade = sheet4.cell(row_index,col_index+1).value
+				if curr_bowler not in total_bowler_grd:
+					total_bowler_grd[curr_bowler] = curr_grade				
+				else:
+					total_bowler_grd[curr_bowler] += 20*curr_grade				
+
+	#Create new sheet
+	workbook,grey_relation_grades_sheet = create_new_sheet(
+														"bowler_final_grey_relation_grades.xls"
+														)
+
+	#Initialize rows,columns
+	row_count = 0		
+	grey_relation_grades_sheet.write(0,0,"Player Name")
+	grey_relation_grades_sheet.write(0,1,"Grade")
+	row_count+=1
+	for x in total_bowler_grd:
+		grey_relation_grades_sheet.write(row_count,0,x)
+		grey_relation_grades_sheet.write(row_count,1,total_bowler_grd[x]/100.0)
+		row_count+=1
+	workbook.close()										
 
 def complete_batsmen_performance():
 	sheet1 = open("grey_relation_grades2011.xls")
@@ -327,21 +600,18 @@ def complete_batsmen_performance():
 												
 
 
-def normalize_complete_bowler_stats(sheet_name):
-	innings = {}
-	wickets = {}
-	economy = {}
-	sr = {}
-	avg = {}
-	fw = {}
-	sheet = open(sheet_name)
-	
 
 
 				
 yrs = [2011,2012,2013,2014]
+#for x in yrs:
+#	normalize_complete_batsmen_stats(x)
+#	batsmen_grc_calculation(x)
+#	batsmen_grd_calculation(x)
+#complete_batsmen_performance()	
+
 for x in yrs:
-	normalize_complete_batsmen_stats(x)
-	batsmen_grc_calculation(x)
-	batsmen_grd_calculation(x)
-complete_batsmen_performance()	
+	normalize_complete_bowler_stats(x)
+	bowler_grc_calculation(x)
+	bowler_grd_calculation(x)
+complete_bowler_performance()	
